e8c73649f19ae8a61438de9d515ccb44a7476d15
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2021-10-07 00:31:36 +0530

    Add files via upload
    Find elements which are present in first array and not in second

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace proj
+{
+    class Check
+    {
+        public void comma(int[] a,int[] b,int m,int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                int j;
+
+                for (j = 0; j < m; j++)
+                    if (a[i] == b[j])
+                        break;
+
+                if (j == m)
+                    Console.Write(a[i] + " ");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4 };
+            int[] arr1 = { 1, 2, 3, 5 };
+            int m = arr.Length;
+            int n = arr1.Length;
+
+            Check ch = new Check();
+            ch.comma(arr, arr1, m, n);
+        }
+    }
+}
